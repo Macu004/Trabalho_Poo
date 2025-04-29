b8a1d3dc5d5c3f6b21fb795405627b06be02d790
Marquin <marcusaureliu04@gmail.com>
2025-04-29 09:11:25 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -8,14 +8,20 @@ public class Estacionamento {
     public boolean entrarVeiculo(Veiculo v) {
         if (vagasOcupadas.size() >= totalVagas) return false;
         for (Veiculo veic : vagasOcupadas) {
-            if (veic.getPlaca().equalsIgnoreCase(v.getPlaca())) return false; // já está dentro
+            if (veic.getPlaca().equalsIgnoreCase(v.getPlaca()) &&
+                veic.getTipo().equalsIgnoreCase(v.getTipo())) {
+                return false;
+            }
         }
         vagasOcupadas.add(v);
         return true;
     }
 
-    public boolean sairVeiculo(String placa) {
-        return vagasOcupadas.removeIf(v -> v.getPlaca().equalsIgnoreCase(placa));
+    public boolean sairVeiculo(String placa, String tipo) {
+        return vagasOcupadas.removeIf(v -> 
+            v.getPlaca().equalsIgnoreCase(placa) && 
+            v.getTipo().equalsIgnoreCase(tipo)
+        );
     }
 
     public int getVagasOcupadas() {
