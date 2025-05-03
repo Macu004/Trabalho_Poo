d846929c39cae9bb162df956b2e16ae8679c5538
Marquin <marcusaureliu04@gmail.com>
2025-05-03 00:34:34 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,9 +1,16 @@
-public class Veiculo {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Veiculo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String placa;
     private String modelo;
     private String tipo;
     private String cidade;
     private String estado;
+    private Date horarioEntrada;
+    private Date horarioSaida;
 
     public Veiculo(String placa, String modelo, String tipo, String cidade, String estado) {
         this.placa = placa;
@@ -12,24 +19,59 @@ public class Veiculo {
         this.cidade = cidade;
         this.estado = estado;
     }
-
     public String getPlaca() {
         return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
 
     public String getModelo() {
         return modelo;
     }
 
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
     public String getTipo() {
         return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public String getCidade() {
         return cidade;
     }
 
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
     public String getEstado() {
         return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getHorarioEntrada() {
+        return horarioEntrada;
+    }
+
+    public void setHorarioEntrada(Date horarioEntrada) {
+        this.horarioEntrada = horarioEntrada;
+    }
+
+    public Date getHorarioSaida() {
+        return horarioSaida;
+    }
+
+    public void setHorarioSaida(Date horarioSaida) {
+        this.horarioSaida = horarioSaida;
     }
 }
